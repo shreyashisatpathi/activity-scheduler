e176281e86626b666720defbef60d62225f59945
Shreyashi <shreyashi04@gmail.com>
2023-06-19 18:58:21 +0200

    added a generic button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,43 @@
+interface ButtonProps {
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({ 
+  label, 
+  onClick, 
+  disabled, 
+  outline,
+  small,
+}) => {
+  return ( 
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      className={`
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition
+        w-full
+        
+        ${outline ? 'bg-white' : 'bg-black-500'}
+        ${outline ? 'border-black' : 'border-black-500'}
+        ${outline ? 'text-black' : 'text-white'}
+        ${small ? 'text-sm' : 'text-md'}
+        ${small ? 'py-1' : 'py-3'}
+        ${small ? 'font-light' : 'font-semibold'}
+        ${small ? 'border-[1px]' : 'border-2'}
+      `}
+    >
+      {label}
+    </button>
+   );
+}
+ 
+export default Button;
